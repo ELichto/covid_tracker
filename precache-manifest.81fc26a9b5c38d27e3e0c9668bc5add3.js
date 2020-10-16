80150f02687354dddd182d072970d6182ee47dbe
ELMallaliSoufiane <elmallali.sufiane@gmail.com>
2020-10-16 14:53:50 +0100

    Updates

---
+++ precache-manifest.81fc26a9b5c38d27e3e0c9668bc5add3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5780d3107cc2dc0ba353c4a80b33e813",
+    "url": "/covid_tracker/index.html"
+  },
+  {
+    "revision": "b3fc0559ca759b80da27",
+    "url": "/covid_tracker/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "14138c8211d397affbf1",
+    "url": "/covid_tracker/static/css/main.98c1c0ae.chunk.css"
+  },
+  {
+    "revision": "b3fc0559ca759b80da27",
+    "url": "/covid_tracker/static/js/2.b0224317.chunk.js"
+  },
+  {
+    "revision": "8b81bc38395cca664a526c4e304a6603",
+    "url": "/covid_tracker/static/js/2.b0224317.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14138c8211d397affbf1",
+    "url": "/covid_tracker/static/js/main.a172dee5.chunk.js"
+  },
+  {
+    "revision": "a883e06c2ae183536200",
+    "url": "/covid_tracker/static/js/runtime-main.aed91c53.js"
+  }
+]);
